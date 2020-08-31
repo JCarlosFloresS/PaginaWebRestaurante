f7873cb1125bff856d8b6748e511ef3ca1055eec
Juan Carlos Flores <69329529+JCarlosFloresS@users.noreply.github.com>
2020-08-31 18:32:11 -0500

    Add files via upload

---
+++ Restaurante/js/efectos.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+    //Efecto menu
+    //Primero guardamos cada uno de los botones en una variable
+    var acercaDe = $('#acerca-de').offset().top,
+        menu = $('#platillos').offset().top,
+        galeria = $('#galeria').offset().top,
+        ubicacion = $('#ubicacion').offset().top;
+
+    //Ahora hacemos que al precionar cada uno de los botones del menu, este nos mande al apartado correspondiente
+    $('#btn-acerca-de').on('click', function(e){
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: 380
+		}, 500);
+	});
+
+	$('#btn-menu').on('click', function(e){
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: menu - 40
+		});
+	});
+
+	$('#btn-galeria').on('click', function(e){
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: galeria - 40
+		});
+	});
+
+	$('#btn-ubicacion').on('click', function(e){
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: ubicacion - 40
+		});
+	});
+
+    //Le colocamos el efecto al menu
+    $('.menu a').each(function(index, elemento){
+		$(this).css({
+			'top': '-200px'
+		});
+
+		$(this).animate({
+			top: '0'
+		},2000 + (index * 500));
+    });
+    
+    //Y al final el efecto del header el cual solo se ejecutara si la pantalla es superior a los 800px
+    if ($(window).width() > 800){
+		$('header .textos').css({
+			opacity: 0,
+			marginTop: 0
+		});
+
+		$('header .textos').animate({
+			opacity: 1,
+			marginTop: '-52px'
+		},1500);
+	}
+   
+});
